909faa9649281c361eb94ef20b7aacc6d415e3a9
Caroline Mimeault <carolinemimeault@gmail.com>
2023-06-11 18:46:25 -0400

    cleaning code to get images

--- get_data.py
+++ get_data.py
@@ -20,16 +20,16 @@ def search_images(term, max_images=30):
 searches = "forest", "bird"
 path = Path("bird_or_not")
 
-for o in searches:
-    dest = path / o
+for term in searches:
+    dest = path / term
     dest.mkdir(exist_ok=True, parents=True)
-    download_images(dest, urls=search_images(f"{o} photo"))
+    download_images(dest, urls=search_images(f"{term} photo"))
     sleep(10)  # Pause between searches to avoid over-loading server
-    download_images(dest, urls=search_images(f"{o} sun photo"))
+    download_images(dest, urls=search_images(f"{term} sun photo"))
     sleep(10)
-    download_images(dest, urls=search_images(f"{o} shade photo"))
+    download_images(dest, urls=search_images(f"{term} shade photo"))
     sleep(10)
-    resize_images(path / o, max_size=400, dest=path / o)
+    resize_images(path / term, max_size=400, dest=path / term)
 
 failed = verify_images(get_image_files(path))
 failed.map(Path.unlink)
