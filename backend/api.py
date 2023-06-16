70d26b3865dbda65b47b5ea1161d352792389bc8
Caroline Mimeault <carolinemimeault@gmail.com>
2023-06-15 22:02:00 -0400

    reorganised with a front and back end

--- backend/api.py
+++ backend/api.py
@@ -1,16 +1,24 @@
 # import libraries
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastai.vision.all import (
     load_learner,
     PILImage,
 )
+from fastapi.middleware.cors import CORSMiddleware
 
 learn = load_learner("model.pkl")
 
 app = FastAPI()
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+)
+
+
 @app.get("/")
 async def root():
     is_bird, _, probs = learn.predict(PILImage.create("forest.jpg"))
-    return {"Probability it's a bird": probs.tolist()[0], "This is a ": is_bird}
+    return {"probability": probs.tolist()[0], "answer": is_bird}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		fetch(`http://localhost:8000`)
+			.then((response) => {
+				return response.json()
+			})
+			.then((actualData) => {
+				console.log({ data: actualData })
+				setData(actualData);
+			})
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Bird or Forest?</h1>
+
+			<form>
+				<p>Upload a picture of a bird or a forest</p>
+				<p>Our very advanced Machine Learning model will tell you what it is</p>
+				<div>
+					<label for="file">Choose file to upload</label>
+					<input type="file" id="file" name="file" accept="image/png, image/jpeg" />
+				</div>
+			</form>
+
+			<div class="departures">
+				<p><em style={{ fontWeight: "bold" }}>Probability that it is a bird: </em>{data && data.probability}</p>
+				<p><em style={{ fontWeight: "bold" }}>It is a </em>{data && data.answer} !</p>
+			</div>
+
+		</div>
+	);
+}
+
+export default App;
